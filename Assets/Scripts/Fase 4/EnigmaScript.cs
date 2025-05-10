8bfda088070f95043772e7bbe6c0329a727d3cc5
Paulo Roberto Fernandes Holanda <paulorfholanda@gmail.com>
2025-05-10 14:50:50 -0300

    ✨ feat: finaliza a base da fase quatro (❤️ ω ❤️)

--- Assets/Scripts/Fase 4/EnigmaScript.cs
+++ Assets/Scripts/Fase 4/EnigmaScript.cs
@@ -1,111 +1,122 @@
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using System.Collections.Generic;
 using System.Collections;
 using System;
+using UnityEngine.Serialization;
 
 public class EnigmaScript : MonoBehaviour
 {
-    public List<Enigma> enigmas;
-    public GameObject puzzleUIPrefab;
-    public Transform parentCanvas;
-    public int cenaVitoria, cenaDerrota;
-    [SerializeField] private GameObject Instruct;
-    private int respostasCorretas = 0;
-    private int totalRespondidos = 0;
-    public AudioClip prologoClip;
-    public AudioSource audioSource;
-    public void SelecionarEnigma(int index)
-    {
-        var go = Instantiate(puzzleUIPrefab, parentCanvas);
-        var ui = go.GetComponent<EnigmaUI>();
-        ui.Inicializar(enigmas[index], OnEnigmaRespondido);
-    }
+	public List<Enigma> enigmas;
+	public GameObject puzzleUIPrefab;
+	public Transform parentCanvas;
+	public int cenaVitoria, cenaDerrota;	
+    [SerializeField] private GameObject instruct;
+	private int respostasCorretas = 0;
+	private int totalRespondidos = 0;
+	public AudioClip prologoClip;
+	public AudioSource audioSource;
+    public Button[] botoesEnigmas;
 
-    private void Start()
+
+    void Start()
     {
         StartCoroutine(RunAudio());
+        // liga cada botão ao índice
+        for (int i = 0; i < botoesEnigmas.Length; i++)
+        {
+            int idx = i;
+            botoesEnigmas[i].onClick.RemoveAllListeners();
+            botoesEnigmas[i].onClick.AddListener(() =>
+                SelecionarEnigma(idx, botoesEnigmas[idx])
+            );
+        }
+    }
+
+    public void SelecionarEnigma(int index, Button origemBtn)
+    {
+        var go = Instantiate(puzzleUIPrefab);
+        go.transform.SetParent(null, false); 
+        Canvas cv = go.GetComponent<Canvas>();
+        cv.renderMode      = RenderMode.ScreenSpaceCamera;
+        cv.worldCamera     = Camera.main;
+        cv.sortingOrder    = 3;
+        go.transform.SetAsLastSibling();
+        var ui = go.GetComponentInChildren<EnigmaUI>();
+        ui.Inicializar(enigmas[index], OnEnigmaRespondido, origemBtn);
     }
 
     private void OnEnigmaRespondido(bool acertou)
-    {
-        totalRespondidos++;
-        if (acertou) respostasCorretas++;
+	{
+		totalRespondidos++;
+		if (acertou) respostasCorretas++;
 
+		if (totalRespondidos >= enigmas.Count)
+		{
 
-        if (totalRespondidos >= enigmas.Count)
-        {
+			if (respostasCorretas >= 2)
+			{
+				if (mainManager.main == null)
+				{
+					mainManager.main = new mainManager();
 
-            if (respostasCorretas >= 2)
-            {
-                if (mainManager.main == null)
-                {
-                    mainManager.main = new mainManager();
-
-                    mainManager.indiceCanvainicial = cenaVitoria;
-                    
                 }
-                else
-                {
-                    mainManager.main.InstanciarCanva(cenaVitoria);
-                }
+                mainManager.indiceCanvainicial = cenaVitoria;
+                SceneManager.LoadScene("main");
+
             }
-            else
-            {
-                if (mainManager.main == null)
-                {
-                    mainManager.main = new mainManager();
-
-                    mainManager.indiceCanvainicial = cenaDerrota;
+			else
+			{
+				if (mainManager.main == null)
+				{
+					mainManager.main = new mainManager();
 
                 }
-                else
-                {
-                    mainManager.main.InstanciarCanva(cenaDerrota);
-                }
+                mainManager.indiceCanvainicial = cenaDerrota;
+                SceneManager.LoadScene("main");
             }
-        }
-    }
+		}
+	}
 
-    public void CloseInstruct()
-    {
-        if (Instruct != null)
-        {
-            Instruct.SetActive(false);
-            Destroy(Instruct);
-        }
-    }
+	public void CloseInstruct()
+	{
+		if (instruct != null)
+		{
+			instruct.SetActive(false);
+			Destroy(instruct);
+		}
+	}
 
-    private IEnumerator RunAudio()
-    {
-        var steps = new List<Func<IEnumerator>>()
-        {
-            () => PrepareAudio(prologoClip),
-        };
+	private IEnumerator RunAudio()
+	{
+		var steps = new List<Func<IEnumerator>>()
+		{
+			() => PrepareAudio(prologoClip),
+		};
 
-        yield return LoadingScreenController.Instance.ShowLoading(steps);
-        audioSource.Play();
-    }
+		yield return LoadingScreenController.Instance.ShowLoading(steps);
+		audioSource.Play();
+	}
 
 
-        private IEnumerator PrepareAudio(AudioClip prologoClip)
-    {
-        if (prologoClip == null)
-        {
-            Debug.LogWarning("PrologoManager: prologoClip n�o atribu�do.");
-            yield break;
-        }
+		private IEnumerator PrepareAudio(AudioClip prologoClip)
+	{
+		if (prologoClip == null)
+		{
+			Debug.LogWarning("PrologoManager: prologoClip não atribuído.");
+			yield break;
+		}
 
-        prologoClip.LoadAudioData();
-        while (prologoClip.loadState != AudioDataLoadState.Loaded)
-        {
-            if (prologoClip.loadState == AudioDataLoadState.Failed)
-            {
-                Debug.LogError("PrologoManager: Falha ao carregar os dados de �udio.");
-                yield break;
-            }
-            yield return null;
-        }
-    }
+		prologoClip.LoadAudioData();
+		while (prologoClip.loadState != AudioDataLoadState.Loaded)
+		{
+			if (prologoClip.loadState == AudioDataLoadState.Failed)
+			{
+				Debug.LogError("PrologoManager: Falha ao carregar os dados de áudio.");
+				yield break;
+			}
+			yield return null;
+		}
+	}
 }
--- Assets/Scripts/Fase 4/EnigmaUI.cs
+++ Assets/Scripts/Fase 4/EnigmaUI.cs
@@ -1,32 +1,41 @@
+using System;
 using UnityEngine;
 using UnityEngine.UI;
-using System;
+using TMPro;
 
 public class EnigmaUI : MonoBehaviour
 {
-    public Text txtPergunta;
+    public GameObject UiGameObject;
+    public TextMeshProUGUI txtPergunta;
     public Button[] botoesAlt;
     public GameObject painelExplic;
-    public Text txtExplicacao;
+    public TextMeshProUGUI txtExplicacao;
     public Color corCerta = Color.green;
     public Color corErrada = Color.red;
-
     private Action<bool> callback;
+    private Button origemButton;
+    public  Button fecharButton;
     private int indiceCorreto;
     private bool respondido = false;
+    private bool acertou;
 
-    public void Inicializar(Enigma e, Action<bool> onFinish)
+
+    public void Inicializar(Enigma e,
+                            Action<bool> onFinish,
+                            Button origemBtn)
     {
-        txtPergunta.text = e.enigma;
-        indiceCorreto = e.idCorreto;
         callback = onFinish;
+        origemButton = origemBtn;
+        indiceCorreto = e.idCorreto;
+        txtPergunta.text = e.enigma;
         txtExplicacao.text = e.explicacao;
         painelExplic.SetActive(false);
 
         for (int i = 0; i < botoesAlt.Length; i++)
         {
             int idx = i;
-            botoesAlt[i].transform.GetChild(0).GetComponent<Text>().text = e.alternativas[i];
+            var txt = botoesAlt[i].GetComponentInChildren<TextMeshProUGUI>();
+            txt.text = e.alternativas[i];
             botoesAlt[i].onClick.RemoveAllListeners();
             botoesAlt[i].onClick.AddListener(() => OnEscolheu(idx));
         }
@@ -36,26 +45,48 @@ public class EnigmaUI : MonoBehaviour
     {
         if (respondido) return;
         respondido = true;
+        acertou = (idxEscolhido == indiceCorreto);
+
         for (int i = 0; i < botoesAlt.Length; i++)
         {
-            var colors = botoesAlt[i].colors;
-            if (i == indiceCorreto) colors.normalColor = corCerta;
-            else if (i == idxEscolhido) colors.normalColor = corErrada;
-            botoesAlt[i].colors = colors;
+           
+            botoesAlt[i].image.color = corErrada;
+            Image cols = botoesAlt[i].GetComponent<Image>();
+            if (i == indiceCorreto) cols.color = corCerta;
+            else if (i == idxEscolhido) cols.color = corErrada;
+            Debug.Log($"botao {i} - {cols.color}");
+            botoesAlt[i].image = cols;
             botoesAlt[i].interactable = false;
         }
+
         Invoke(nameof(MostrarExplicacao), 1f);
     }
 
     private void MostrarExplicacao()
     {
-        painelExplic.SetActive(true);
-        var btn = painelExplic.GetComponentInChildren<Button>();
-        btn.onClick.RemoveAllListeners();
-        btn.onClick.AddListener(() =>
+        if (UiGameObject == null)
         {
-            callback( /* acertou? */ respondido && (Array.IndexOf(botoesAlt, botoesAlt[indiceCorreto]) == indiceCorreto) );
-            Destroy(gameObject);
-        });
+            UiGameObject = GameObject.FindWithTag("EnigmaPainel");
+            if (UiGameObject == null)
+            {
+                Debug.LogError("UiGameObject não encontrado!");
+                return;
+            }
+        }
+        painelExplic.SetActive(true);
+        if (fecharButton == null)
+        {
+            fecharButton = painelExplic.GetComponentInChildren<Button>();
+        }
+        fecharButton.onClick.RemoveAllListeners();
+         fecharButton.onClick.AddListener(() =>
+        {
+            Image cols = origemButton.GetComponent<Image>();
+        cols.color = acertou ? corCerta : corErrada;
+            origemButton.image = cols;
+            origemButton.interactable = false;
+            callback(acertou);
+            Destroy(UiGameObject);
+        }); 
     }
 }
